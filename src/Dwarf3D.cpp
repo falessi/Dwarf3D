7ba4ff219d40aa95d9116ba02ffe73f64e33855b
falessi <falessi@users.noreply.github.com>
2015-08-19 16:24:23 +0200

    Create Dwarf3D.cpp

---
+++ src/Dwarf3D.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Dwarf3D.cpp
+// Author      : 
+// Version     :
+// Copyright   : falessi
+// Description : Rendering Engine in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
